e7380003cce090ba3aa7b4697aaaa4d5989952d4
sharad1996 <sharadwankhade23@gmail.com>
2023-04-15 13:55:12 +0530

    Some work

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
-import { Button } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
 import { useState } from 'react';
 import { PieChart, Pie, Cell,Tooltip} from 'recharts';
 import axios from "axios";
+import useStyles from "./Styles";
 
 function App() {
+  const styles = useStyles();
   const [file, setFile] = useState("");
   const [data, setData] = useState(null);
   const onHandleChange = (e) => {
@@ -28,40 +30,56 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {
-        data && (
-          <PieChart width={1000} height={400}>
-            <Pie
-              dataKey="value"
-              isAnimationActive={false}
-              data={data}
-              cx={200}
-              cy={200}
-              outerRadius={80}
-              fill="#8884d8"
-              label
-            />
+    <Box className={styles.container} sx={{
+
+    }}>
+      <Box>
+        <form action="#">
+            <Box className="flex">
+              <Box sx={{ marginBottom: 1}}>
+                <label htmlFor="file">
+                  <Typography variant='h3'> Import your file </Typography>  
+                </label>
+                   </Box>
+              <Box>
+                <input
+                  type="file"
+                  id="file"
+                  // accept=".jpg"
+                  onChange={event => {
+                    const file = event.target.files[0];
+                    setFile(file);
+                  }}
+                />
+              </Box>
+            </Box>
+          </form>
+          <Button sx={{ marginTop: 2}} variant='contained' size='large' onClick={onHandleSubmit}> Import </Button>
+      </Box>
+      <Box>
+        {
+          data && (
+            <PieChart width={1000} height={400}>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={data}
+                cx={200}
+                cy={200}
+                outerRadius={80}
+                fill="#8884d8"
+                label
+              >
+                {data.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+            </Pie>
             <Tooltip />
-        </PieChart>
-        )
-      }
-      <form action="#">
-          <div className="flex">
-            <label htmlFor="file">File</label>
-            <input
-              type="file"
-              id="file"
-              // accept=".jpg"
-              onChange={event => {
-                const file = event.target.files[0];
-                setFile(file);
-              }}
-            />
-          </div>
-        </form>
-        <Button onClick={onHandleSubmit}> Import </Button>
-    </div>
+          </PieChart>
+          )
+        }
+      </Box>
+    </Box>
   );
 }
 
---
+++ front/src/Styles.js
@@ -0,0 +1,12 @@
+import { createStyles, Theme } from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles(() =>
+  createStyles({
+    container: {
+      textAlign: 'center',
+      padding: 100,
+      // maxWidth: 200,
+    }
+  })
+);
